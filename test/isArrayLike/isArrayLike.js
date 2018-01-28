272737bc26c6c7043064ee3c120f4328019a1a84
Awesome Code <team@xinminlabs.com>
2018-01-28 13:58:47 +0000

    Auto corrected by following Whitespace

--- test/isArrayLike/isArrayLike.js
+++ test/isArrayLike/isArrayLike.js
@@ -1,8 +1,8 @@
 const isArrayLike = val => {
-try {
-return [...val], true;
-} catch (e) {
-return false;
-}
+  try {
+    return [...val], true;
+  } catch (e) {
+    return false;
+  }
 };
 module.exports = isArrayLike
