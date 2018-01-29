f0dd138ca214ef49a95ef7151567a7b327879e1a
Awesome Code <team@xinminlabs.com>
2018-01-29 02:25:48 +0000

    Auto corrected by following Whitespace

--- test/bindKey/bindKey.js
+++ test/bindKey/bindKey.js
@@ -1,5 +1,5 @@
 const bindKey = (context, fn, ...args) =>
-function() {
-return context[fn].apply(context, args.concat(...arguments));
-};
+  function() {
+    return context[fn].apply(context, args.concat(...arguments));
+  };
 module.exports = bindKey
--- test/difference/difference.js
+++ test/difference/difference.js
@@ -1,5 +1,5 @@
 const difference = (a, b) => {
-const s = new Set(b);
-return a.filter(x => !s.has(x));
+  const s = new Set(b);
+  return a.filter(x => !s.has(x));
 };
 module.exports = difference
